479771e2ad770ed05b3098c2eab40f25e1b1edea
nar2cis2sism <nar2cis2sism@163.com>
2018-11-13 16:57:06 +0800

    java引擎增加socket模块

---
+++ JavaEngine/src/engine/java/socket/util/SocketUtil.java
@@ -0,0 +1,34 @@
+package engine.java.socket.util;
+
+import engine.java.util.secure.CRCUtil;
+import engine.java.util.secure.Obfuscate;
+
+public final class SocketUtil {
+
+    private static byte[] crypt_key;                                // 数据密钥
+
+    /**
+     * 握手
+     * 
+     * @param bs 握手信息
+     * @return CRC校验值
+     */
+    public static int handshake(byte[] bs) {
+        // 解密握手信息
+        crypt_key = Obfuscate.clarify(bs);
+        return CRCUtil.calculate(crypt_key, crypt_key.length);
+    }
+
+    /**
+     * 数据处理
+     */
+    public static void crypt(byte[] data) {
+        if (crypt_key != null && data != null)
+        {
+            for (int i = 0; i < data.length; i++)
+            {
+                data[i] = (byte) (data[i] ^ crypt_key[i % crypt_key.length]);
+            }
+        }
+    }
+}
